1f452409fc2c9e0d4444f4df72f96055fbe7fb68
Frode Hansen <frode.hansen@bekk.no>
2018-03-28 08:55:51 +0200

    Slette dist filer

--- dist/elementer/AvgrensningerInfo.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import * as React from 'react';
-import { DatovelgerAvgrensninger } from '../types';
-export interface Props {
-    id: string;
-    avgrensninger: DatovelgerAvgrensninger;
-}
-declare const AvgrensningerInfo: React.StatelessComponent<Props>;
-export default AvgrensningerInfo;
--- dist/kalender/localeUtils.d.ts
+++
@@ -1,3 +0,0 @@
-import { LocaleUtils } from 'react-day-picker/types/utils';
-declare const kalenderLocaleUtils: LocaleUtils;
-export default kalenderLocaleUtils;
