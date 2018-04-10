7257ec698c051a514610c7b9320e462fca762d7a
Frode Hansen <frode.hansen@bekk.no>
2018-04-10 08:34:43 +0200

    1.0.1 utvide med dayPickerProps

--- app/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import DatovelgerApp from './App';
-
-ReactDOM.render(<DatovelgerApp />, document.getElementById(
-	'root'
-) as HTMLElement);
