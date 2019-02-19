c681965672362aed10b3432739c9e667b5c05d13
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-02-19 09:56:57 +0100

    Remove dist

--- dist/datovelger/utils/datovalidering.d.ts
+++
@@ -1,4 +0,0 @@
-import { Avgrensninger, Tidsperiode } from '../types';
-import * as moment from 'moment';
-export declare const erDatoGyldig: (dato: string, avgrensninger?: Avgrensninger) => boolean;
-export declare const erDatoIEnUgyldigTidsperiode: (dato: moment.Moment, ugyldigeTidsperioder: Tidsperiode[]) => boolean;
--- dist/datovelger/utils/kalenderFokusUtils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const getFokusertDato: (kalender: HTMLElement) => Date;
-export declare const getSammeDatoIMåned: (dato: Date, måned: Date, nesteMåned: Date) => Date;
-export declare const fokuserPåDato: (kalender: HTMLElement, dato: Date) => void;
-export declare const fokuserKalender: (kalender: HTMLElement) => void;
