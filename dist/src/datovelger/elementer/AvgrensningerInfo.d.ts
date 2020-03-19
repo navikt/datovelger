bd90b7def7b45cbf2513f8c9a10aba3831bd503b
Frode Hansen <frode.hansen@bekk.no>
2020-03-19 21:17:51 +0100

    Updates

--- dist/src/datovelger/elementer/AvgrensningerInfo.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { Avgrensninger } from '../types';
-export interface Props {
-    id: string;
-    avgrensninger: Avgrensninger;
-}
-declare const AvgrensningerInfo: React.StatelessComponent<Props>;
-export default AvgrensningerInfo;
--- dist/src/datovelger/elementer/ChevronSvg.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type ChevronRetning = 'opp' | 'ned' | 'høyre' | 'venstre';
-export interface Props {
-    retning?: ChevronRetning;
-}
-declare const Chevron: (props: Props) => JSX.Element;
-export default Chevron;
