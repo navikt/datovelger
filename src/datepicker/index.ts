91fa0feaf207b5a073258c67d522a20184373b97
Frode Hansen <frode.hansen@bekk.no>
2020-10-15 11:37:44 +0200

    Update index exports

--- src/datepicker/index.ts
+++ src/datepicker/index.ts
@@ -1,3 +1,5 @@
 export { default as Datepicker } from './Datepicker';
 
-export * from './types';
+export { isISODateString } from './types/typeGuards';
+
+export type { CalendarPlacement, DatepickerDateRange, DatepickerLimitations } from './types';
