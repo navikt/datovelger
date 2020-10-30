2f6771dc40d3c65ba5b78ca8877791f2eb02b9b8
Frode Hansen <frode.hansen@bekk.no>
2020-10-30 11:06:13 +0100

    Add parseTwoDigitYear rule for moment

--- src/datepicker/utils/dateFormatUtils.ts
+++ src/datepicker/utils/dateFormatUtils.ts
@@ -5,6 +5,11 @@ export const INVALID_DATE_VALUE = 'Invalid date';
 export const INPUT_DATE_STRING_FORMAT: InputDateString = 'DD.MM.YYYY';
 export const ISO_DATE_STRING_FORMAT: ISODateString = moment.HTML5_FMT.DATE;
 
+moment.parseTwoDigitYear = function (yearString) {
+    const thisYear = new Date().getFullYear() / 100;
+    return parseInt(yearString) + (parseInt(yearString) > thisYear ? 1900 : 2000);
+};
+
 const ALLOWED_INPUT_FORMATS = [INPUT_DATE_STRING_FORMAT, 'DDMMYYYY', 'DD/MM/YYYY', 'DD-MM-YYYY', 'DDMMYY', 'D.M.YY'];
 
 const stringToUTCDate = (dateString: string | undefined, format: string): Date | undefined => {
